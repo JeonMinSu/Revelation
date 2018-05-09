16a8b34644166b8627187c4b68c414138d604dc2
SamMul <815youngmin@naver.com>
2018-05-09 16:19:39 +0900

    매니저들

---
+++ TestResources/KYM/UtilityManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 작성일 : 2018 - 05 - 09
+/// 직상지 : 김영민
+/// 작업내용 : 유틸리티 매니저 제작
+/// 다른곳에서 자주 불리는 애들 또는 자주 하는 계산 여기서 가져다 놓고 사용
+/// </summary>
+
+public class UtilityManager :  Singleton<UtilityManager>
+{
+    [SerializeField] private GameObject Player;
+    [SerializeField] private GameObject Dragon;
+
+    public Vector3 PlayerPosition() { return Player.transform.position; }
+    public Vector3 DragonPosition() { return Dragon.transform.position; }
+
+    public Transform PlayerTransform() { return Player.transform; }
+    public Transform DragonTransform() { return Dragon.transform; }
+}

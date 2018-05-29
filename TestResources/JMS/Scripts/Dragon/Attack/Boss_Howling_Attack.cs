49d619ac03e2fe2a345b6fb9905473ab9f0d65dc
JeonMinSu <session_min@naver.com>
2018-05-30 01:10:41 +0900

    2018-05-30 커커커미미시밋밋

--- TestResources/JMS/Scripts/Dragon/Attack/Boss_Howling_Attack.cs
+++ TestResources/JMS/Scripts/Dragon/Attack/Boss_Howling_Attack.cs
@@ -23,6 +23,8 @@ public class Boss_Howling_Attack : ActionTask
 
     IEnumerator HowlingAttackCor(float preTime, float afterTime)
     {
+        Transform Boss = UtilityManager.Instance.DragonTransform();
+        Transform Player = UtilityManager.Instance.PlayerTransform();
 
         BlackBoard.Instance.IsGroundAttacking = true;
         BlackBoard.Instance.IsRoarAttacking = true;
@@ -31,6 +33,22 @@ public class Boss_Howling_Attack : ActionTask
 
         Transform mouth = BlackBoard.Instance.DragonMouth;
 
+        Vector3 forward = (Player.position - Boss.position).normalized;
+
+        while (Vector3.Dot(Boss.forward, forward) < 0.99f)
+        {
+            Boss.rotation =
+                Quaternion.RotateTowards(
+                    Boss.rotation,
+                    Quaternion.LookRotation(forward, Vector3.up),
+                    180.0f * Time.deltaTime
+                    );
+
+            Debug.Log("No rotation");
+
+            yield return CoroutineManager.FiexdUpdate;
+        }
+
         //선딜
         yield return CoroutineManager.GetWaitForSeconds(new WaitForSeconds(preTime));
 
@@ -38,7 +56,11 @@ public class Boss_Howling_Attack : ActionTask
 
         BulletManager.Instance.CreateDragonHomingBullet(mouth.position, (mouth.forward + Vector3.up * 3).normalized);
         BulletManager.Instance.CreateDragonHomingBullet(mouth.position, (mouth.forward + mouth.right * 10.0f + Vector3.up * 3).normalized);
+        BulletManager.Instance.CreateDragonHomingBullet(mouth.position, (mouth.forward + mouth.right * 5.0f + Vector3.up * 4).normalized);
+
         BulletManager.Instance.CreateDragonHomingBullet(mouth.position, (mouth.forward - mouth.right * 10.0f + Vector3.up * 3).normalized);
+        BulletManager.Instance.CreateDragonHomingBullet(mouth.position, (mouth.forward - mouth.right * 5.0f + Vector3.up * 4).normalized);
+
 
         yield return CoroutineManager.GetWaitForSeconds(new WaitForSeconds(runTime));
 
